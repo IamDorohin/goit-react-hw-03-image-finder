319f3bb5e6e296e20d06b428789026d11bafec26
Kostiantyn Dorohin <iamdorohin@gmail.com>
2022-11-14 23:28:24 +0200

    Project clear

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,30 +42,6 @@ export class App extends Component {
         this.setState({ status: 'resolved' });
       }
     }
-    // const { queryPage, searchQuery } = this.state;
-
-    // if (
-    //   prevState.searchQuery !== searchQuery ||
-    //   prevState.queryPage !== queryPage
-    // ) {
-    //   this.setState({ loading: true });
-
-    //   try {
-    //     const data = await fetchAPI(searchQuery, queryPage);
-
-    //     this.setState(prevState => ({
-    //       responseArray: [...prevState.responseArray, ...data.hits],
-    //     }));
-
-    //     if (queryPage === Math.ceil(data.totalHits / 12)) {
-    //       this.setState({ lastPage: true });
-    //     }
-    //   } catch (error) {
-    //     this.setState({ error: true, loading: false });
-    //   } finally {
-    //     this.setState({ loading: false });
-    //   }
-    // }
   }
 
   handleSearchQueryChange = searchQuery => {
@@ -77,27 +53,6 @@ export class App extends Component {
       return { queryPage: prevState.queryPage + 1 };
     });
   };
-
-  // render() {
-  //   const { loading, queryPage, responseArray, error, searchQuery, lastPage } =
-  //     this.state;
-
-  //   return (
-  //     <MainContainer>
-  //       <Searchbar onSubmit={this.handleSearchQueryChange} />
-  //       {loading && <Loader />}
-  //       {queryPage ? (
-  //         <ImageGallery responseArray={responseArray} />
-  //       ) : (
-  //         <EmptyList>You haven't made your first request yet</EmptyList>
-  //       )}
-  //       {error && <EmptyList>{error.message}</EmptyList>}
-  //       {searchQuery !== '' &&
-  //         !loading &&
-  //         !lastPage(<Button onClick={this.onLoadMore}>Load more</Button>)}
-  //     </MainContainer>
-  //   );
-  // }
 
   render() {
     const { queryPage, responseArray, error, lastPage, status } = this.state;
@@ -119,27 +74,3 @@ export class App extends Component {
     );
   }
 }
-
-// const KEY = '30234526-30dbaada1436fb2bf1e0a6a2b';
-
-// fetch(
-//   `https://pixabay.com/api/?q=${this.state.searchQuery}&page=${this.state.queryPage}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
-// )
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     this.setState(prevState => {
-//       return {
-//         responseArray: [...prevState.responseArray, ...data.hits],
-//         // lastPageNumber: Math.ceil(data.totalHits / 12),
-//       };
-//     });
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-//   .finally(() => this.setState({ loading: false }));
